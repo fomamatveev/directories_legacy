651cbfa5839dcfc79469ba853a6a1ce0e14d4424
m.meshkov <m.meshkov@bars.group>
2025-03-22 00:08:23 +0300

    delete

---
+++ DL.Core/Extensions/ClaimPrincipalExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Claims;
+
+namespace DL.Core.Extensions;
+
+/// <summary>
+/// Расширение для получение идентификатора пользователя
+/// </summary>
+public static class ClaimPrincipalExtensions
+{
+    public static long? TryGetUserId(this ClaimsPrincipal claimsPrincipal)
+    {
+        var claim = claimsPrincipal.FindFirst("id");
+
+        if (claim == null)
+        {
+            return null;
+        }
+
+        return long.TryParse(claim.Value, out var id) ? id : null;
+    }
+
+    public static long GetUserId(this ClaimsPrincipal claimsPrincipal) =>
+        TryGetUserId(claimsPrincipal) ?? throw new Exception("Пользователь не найден.");
+}
